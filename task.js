e9d6168cdc96631aa1c5499938e955fdf140289d
Andrey Glumov <gluman@yandex.ru>
2025-01-26 20:46:22 +0300

    first commit

---
+++ task.js
@@ -0,0 +1,11 @@
+// some code
+const book_control = document.getElementsByClassName('book__control')
+const book_content = document.getElementsByClassName('book__content')
+ console.log(book_control)
+
+ book_control.array.forEach(element => {
+    
+        element.eventLinster('click', ()=> {
+
+    })
+ });
