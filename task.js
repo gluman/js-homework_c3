e157c1fdeabb1dfe56e43f4901d60b03c70b90ac
Andrey Glumov <gluman@yandex.ru>
2025-01-26 23:32:58 +0300

    create some code

--- task.js
+++ task.js
@@ -1,11 +1,49 @@
 // some code
-const book_control = document.getElementsByClassName('book__control')
+const book_control = document.getElementsByClassName('font-size')
 const book_content = document.getElementsByClassName('book__content')
- console.log(book_control)
+console.log(book_control)
+console.log(book_content)
 
- book_control.array.forEach(element => {
-    
-        element.eventLinster('click', ()=> {
+Array.from(book_control).forEach((e) => {
+    for(i=0; i < Array.from(book_control).length; i++){
+        if(!this.indexOf(e) === i){
+            e.classList.remove('font-size_active')
+        } 
+    }
+    this.indexOf        
+    e.addEventListener('click', () => {
+        // e.forEach(() => {
+        //     e.classList.remove('font-size_active')
+        //     console.log('удалили font-size_active')
+        // })
+        
+        e.classList.add('font-size_active')
+        console.log('добавили font-size_active')
+        size = e.getAttribute('data-size')
+        console.log('добавили size', size)
+        activeIndex = Array.from(book_control).indexOf(e);
+        // if (size = 'small') {
+        //     classValue = 'font-size_small'
+        //     console.log(classValue)
+        // }
+        // else if (size = 'big') {
+        //     classValue = 'font-size_big'
+        //     console.log(classValue)
+        // }
+        // else {
+        //     classValue = ''
+        //     console.log(classValue)
+        // }
+        // book_content[0].classList.remove('font-size_big')
+        // book_content[0].classList.remove('font-size_small')
+        // book_content.classList.remove('font-size_big')
+        // book_content.classList.remove('font-size_small')
+        // })
 
+        book_content[0].setAttribute('div', classValue)
     })
- });
+})
+
+
+// book_content.classList.add()
+
