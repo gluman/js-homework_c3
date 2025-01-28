dd0e58dbe118bc12ad018f57154613fe1cb4585a
Andrey Glumov <gluman@yandex.ru>
2025-01-28 17:02:12 +0300

    first commit today

--- task.js
+++ task.js
@@ -1,49 +1,34 @@
-// some code
-const book_control = document.getElementsByClassName('font-size')
-const book_content = document.getElementsByClassName('book__content')
-console.log(book_control)
-console.log(book_content)
+const book_controls = document.getElementsByClassName('font-size');
+const book_content = document.getElementsByClassName('book__content')[0];
 
-Array.from(book_control).forEach((e) => {
-    for(i=0; i < Array.from(book_control).length; i++){
-        if(!this.indexOf(e) === i){
-            e.classList.remove('font-size_active')
-        } 
-    }
-    this.indexOf        
-    e.addEventListener('click', () => {
-        // e.forEach(() => {
-        //     e.classList.remove('font-size_active')
-        //     console.log('удалили font-size_active')
-        // })
-        
-        e.classList.add('font-size_active')
-        console.log('добавили font-size_active')
-        size = e.getAttribute('data-size')
-        console.log('добавили size', size)
-        activeIndex = Array.from(book_control).indexOf(e);
-        // if (size = 'small') {
-        //     classValue = 'font-size_small'
-        //     console.log(classValue)
-        // }
-        // else if (size = 'big') {
-        //     classValue = 'font-size_big'
-        //     console.log(classValue)
-        // }
-        // else {
-        //     classValue = ''
-        //     console.log(classValue)
-        // }
-        // book_content[0].classList.remove('font-size_big')
-        // book_content[0].classList.remove('font-size_small')
-        // book_content.classList.remove('font-size_big')
-        // book_content.classList.remove('font-size_small')
-        // })
-
-        book_content[0].setAttribute('div', classValue)
-    })
-})
+let defaul_fontsize = document.querySelectorAll('p')
 
 
-// book_content.classList.add()
+console.log(book_controls);
+console.log(book_content);
+console.log(defaul_fontsize)
+Array.from(book_controls).forEach((control, index) => {
+    control.addEventListener('clickUp', () => {
+        event.preventDefault()
+        let size = control.getAttribute('data-size');
+        console.log('data-size:', size);
+                
+        Array.from(book_controls).forEach(item =>{
+            item.classList.remove('font-size_active')
+        })
 
+        control.classList.add('font-size_active');
+        if (size.includes('small')) {
+            book_content.classList.remove('font-size_big');
+            book_content.classList.add('font-size_small');
+        } else if (size.includes('big')) {
+            book_content.classList.remove('font-size_small');
+            book_content.classList.add('font-size_big');
+        } else {
+            // book_content.restoreState();
+            // book_content.classList.remove('font-size_big');
+            // book_content.classList.remove('font-size_small');
+            // book_content.preventDefault()
+        }
+    });
+});
