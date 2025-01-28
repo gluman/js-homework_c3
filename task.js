37717ab5d70de37fa9d3d93b11960cd23b30689e
Andrey Glumov <gluman@yandex.ru>
2025-01-28 19:44:34 +0300

    complete firts task

--- task.js
+++ task.js
@@ -1,33 +1,30 @@
-const book_controls = document.getElementsByClassName('font-size');
-const book_content = document.getElementsByClassName('book__content')[0];
-
-let defaul_fontsize = document.querySelectorAll('p')
-
+let book_controls = document.getElementsByClassName('font-size');
+let book_content = document.getElementsByClassName('book__content');
 
 console.log(book_controls);
 console.log(book_content);
-console.log(defaul_fontsize)
-Array.from(book_controls).forEach((control, index) => {
-    control.addEventListener('clickUp', () => {
-        event.preventDefault()
+// console.log(defaul_fontsize)
+Array.from(book_controls).forEach(control => {
+    control.addEventListener('click', () => {
+        event.preventDefault();
         let size = control.getAttribute('data-size');
         console.log('data-size:', size);
-                
+
         Array.from(book_controls).forEach(item =>{
             item.classList.remove('font-size_active')
         })
 
         control.classList.add('font-size_active');
-        if (size.includes('small')) {
-            book_content.classList.remove('font-size_big');
-            book_content.classList.add('font-size_small');
-        } else if (size.includes('big')) {
-            book_content.classList.remove('font-size_small');
-            book_content.classList.add('font-size_big');
+        if (size === 'small') {
+            book_content[0].classList.remove('font-size_big');
+            book_content[0].classList.add('font-size_small');
+        } else if (size === 'big') {
+            book_content[0].classList.remove('font-size_small');
+            book_content[0].classList.add('font-size_big');
         } else {
             // book_content.restoreState();
-            // book_content.classList.remove('font-size_big');
-            // book_content.classList.remove('font-size_small');
+            book_content[0].classList.remove('font-size_big');
+            book_content[0].classList.remove('font-size_small');
             // book_content.preventDefault()
         }
     });
